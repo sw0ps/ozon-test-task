4b4d414b5aa2df2c7f7b087ea400c89df989d051
sw0ps <dimausachovwv@gmail.com>
2021-08-01 17:49:41 +0300

    refactor: upgrade tests format with more descriptions

--- src/test.php
+++ src/test.php
@@ -3,6 +3,19 @@
 require_once __DIR__ . "/maxOnesAfterRemoveItem.php";
 require_once __DIR__ . "/helper.php";
 
+
+/**
+ * Functions arguments and expected value
+ */
+const testCases = [
+    [[0, 0], 0],
+    [[0, 1], 1],
+    [[1, 0], 1],
+    [[1, 1], 1],
+    [[1, 1, 0, 1, 1], 4],
+    [[1, 1, 0, 1, 1, 0, 1, 1, 1], 5],
+    [[1, 1, 0, 1, 1, 0, 1, 1, 1, 0], 5]
+];
 //
 // Logic for test
 //
@@ -20,19 +33,33 @@ function assertResult(int $result, int $expected, array &$errors)
 }
 
 /**
+ * @param int $result
+ * @param array $params
+ * @param int $expected
+ * @param array $errors
+ */
+function assertEqual(int $result, array $params, int $expected, array &$errors): void
+{
+    if ($result !== $expected) {
+        array_push($errors, "!!! Error: Call func with params [" . arrayToString($params) . "] has result " . $result . " not equal to expected value ". $expected);
+    }
+}
+
+function arrayToString(array $arr): string
+{
+    return implode(", ", $arr);
+}
+
+/**
  * Run for test main function
  */
 function runTest(): void
 {
     $errors = [];
 
-    assertResult(maxOnesAfterRemoveItem([0, 0]), 0, $errors);
-    assertResult(maxOnesAfterRemoveItem([0, 1]), 1, $errors);
-    assertResult(maxOnesAfterRemoveItem([1, 0]), 1, $errors);
-    assertResult(maxOnesAfterRemoveItem([1, 1]), 1, $errors);
-    assertResult(maxOnesAfterRemoveItem([1, 1, 0, 1, 1]), 4, $errors);
-    assertResult(maxOnesAfterRemoveItem([1, 1, 0, 1, 1, 0, 1, 1, 1]), 5, $errors);
-    assertResult(maxOnesAfterRemoveItem([1, 1, 0, 1, 1, 0, 1, 1, 1, 0]), 5, $errors);
+    foreach (testCases as [$params, $expected]) {
+        assertEqual(maxOnesAfterRemoveItem($params), $params, $expected, $errors);
+    }
 
     processingErrorsList($errors);
 }
@@ -47,7 +74,7 @@ function processingErrorsList(array $errors): void
         return;
     }
 
-    foreach ($errors as $key => $error) {
-        println($key . " - " . $error);
+    foreach ($errors as $error) {
+        println($error);
     }
 }
